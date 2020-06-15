4c6296bfb18eeaa8639f01d3614bffacbd5701f6
iahsanujunda <the.zlood@gmail.com>
2020-06-15 22:03:39 +0700

    CHORE: remove shop data

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,10 +1,4 @@
-import SHOP_DATA from './shop.data';
-
 import { ShopActionTypes } from "./shop.types";
-
-const INITIAL_STATE = {
-  collections: SHOP_DATA
-}
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
