6d905a2f2bcd2de31f92f93280375fe0b4fdbeb1
iahsanujunda <the.zlood@gmail.com>
2020-07-16 23:27:39 +0700

    CLN: refactor fetching firestore to shopSagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import { ShopActionTypes } from './shop.types';
 import {fetchCollectionsFailed, fetchCollectionsSuccess} from './shop.actions';
@@ -24,4 +24,8 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   )
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)])
 }
